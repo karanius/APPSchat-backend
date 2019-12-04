768850960de9a0377c30fc983c28b4ef226727f7
Kavian Darvish <kavian1991@gmail.com>
2019-12-04 12:58:43 -0500

    uploading the backend

---
+++ index.js
@@ -0,0 +1,48 @@
+const app = require('express')();
+const socketio = require('socket.io');
+
+// this is the port number where the front end will connect to. Currently, it is set up to use port 8080 or Heroku's chosen port automatically.
+const io = socketio(app.listen(process.env.PORT || 8080)); 
+
+
+const onlineUsers = {};
+
+io.on('connect', (socket)=>{
+    socket.emit('userId' , socket.id );
+
+
+    socket.on('myConnectionInfo',(data)=>{
+        onlineUsers[data.id] = data.name;
+        io.emit('userOnline', {user: onlineUsers[data.id] , id:data.id })
+        io.emit('activeUsers', {data:onlineUsers})
+    })
+
+
+
+    socket.on('sendingMsgToServer', (data)=>{
+        io.emit('msgRecievedFromServer' , {data:data} )
+    } )
+
+
+    socket.on('disconnecting',()=>{
+        let rooms = Object.keys(socket.rooms);
+
+        console.log(onlineUsers[rooms],rooms[0],' left')
+        io.emit('userLeft', {user: onlineUsers[rooms], id:rooms[0]})
+        delete onlineUsers[rooms[0]]
+        console.log('online:',onlineUsers)
+        io.emit('activeUsers', {data:onlineUsers})
+    })
+
+
+} )
+
+
+
+
+
+
+
+
+
+console.log('ready')
